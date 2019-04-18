f1208b0e85b532c87a1c9c128832ab59031452a6
Michael Eichberg <mail@michael-eichberg.de>
2019-04-18 10:57:25 +0200

    Live coding code

---
+++ 2019/OPAL/Code/build.sbt
@@ -0,0 +1,17 @@
+name                            := "OPAL Tutorial"
+
+organization    in ThisBuild 	:= "de.tud.stg"
+version         in ThisBuild 	:= "0.1.0-SNAPSHOT"
+scalaVersion    in ThisBuild 	:= "2.12.8"
+
+// fork         in run          := true
+// javaOptions 	in run          += "-Xmx4G"        // BETTER: javaOptions in run += "-Xmx16G"
+// javaOptions 	in run          += "-Xms2G"
+// javaOptions 	in ThisBuild    ++= Seq(
+// 	 "-Dorg.opalj.threads.CPUBoundTasks=2",        // Number of physical (not hyperthreaded) cores/CPUs
+// 	 "-Dorg.opalj.threads.IOBoundTasks=3"          // Number of (hyperthreaded) cores * [1,5...3]
+// )
+
+resolvers in ThisBuild ++= Seq(Opts.resolver.sonatypeSnapshots)
+
+libraryDependencies += "de.opal-project" %% "opal-developer-tools" % "3.0.0-SNAPSHOT" withJavadoc() withSources()
---
+++ 2019/OPAL/Code/src/main/scala/de/tud/stg/br/DoNotInvokeThreadRun.scala
@@ -0,0 +1,70 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package de.tud.stg
+
+package br
+
+import java.net.URL
+import scala.collection.JavaConverters._
+
+import org.opalj.util.PerformanceEvaluation
+import org.opalj.br._
+import org.opalj.br.analyses._
+import org.opalj.br.instructions._
+
+object DoNotInvokeThreadRun extends DefaultOneStepAnalysis {
+
+  override def description: String = "Finds Thread.run calls."
+
+  final val ThreadType = ObjectType("java/lang/Thread")
+
+  def doAnalyze(p: Project[URL], params: Seq[String], isInterrupted: () ⇒ Boolean): BasicReport = {
+    val ch = p.classHierarchy
+
+    def violationsUsingForComprehension(): String = {
+      val violations =
+        for {
+          m <- p.allMethodsWithBody.par
+          c = m.body.get
+          INVOKEVIRTUAL(declClass, "run", MethodDescriptor.NoArgsAndReturnVoid) <- c.iterator
+          if ch.isSubtypeOf(declClass, ThreadType)
+        } yield {
+          m.toJava(s"call to run method of subtype of Thread: " + declClass.toJava)
+        }
+      violations.mkString("\n")
+    }
+
+    def violationsUsingHigherOrderFunction(): String = {
+      val violations = new java.util.concurrent.ConcurrentLinkedQueue[String]()
+      p.parForeachMethodWithBody(isInterrupted) { mi ⇒
+        val m = mi.method
+        val violatingCalls = m.body.get.collect {
+          case INVOKEVIRTUAL(declClass, "run", MethodDescriptor.NoArgsAndReturnVoid)
+              if (ch.isSubtypeOf(declClass, ThreadType)) ⇒
+            declClass
+        }
+        violatingCalls.foreach { case PCAndAnyRef(pc, declClass) ⇒
+            val msg = m.toJava(s"$pc: call to run method of subtype of Thread: " + declClass.toJava)
+            violations.add(msg)
+        }
+      }
+      violations.asScala.mkString("\n")
+    }
+
+    val r2 = PerformanceEvaluation.time(3, 15, 8, violationsUsingHigherOrderFunction, true) { (ns, nss) ⇒
+      val considered = nss.map(_.toSeconds).mkString("[", ", ","]")
+      println(s"violationsUsingHigherOrderFunction took ${ns.toSeconds} $considered")
+    }
+
+    val r1 = PerformanceEvaluation.time(3, 15, 8, violationsUsingForComprehension, true) { (ns, nss) ⇒
+      val considered = nss.map(_.toSeconds).mkString("[", ", ","]")
+      println(s"violationsUsingForComprehension took ${ns.toSeconds} $considered")
+    }
+
+    // on a 6-core CPU...:
+    //  - the ".par" version is roughly 4 times faster than the seq version
+    //  - the "parForeachMethodWithBody" version is roughly 10 times faster than the seq version
+
+    if(r1.lines.size != r2.lines.size) throw new UnknownError(s"$r1 != $r2")
+    r1
+  }
+}
